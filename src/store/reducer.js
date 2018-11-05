3decb0bd708b5125bd47865fdf2a4cfdcb4d4638
abid <fmaabid@gmail.com>
2018-11-05 20:14:16 +0600

    initialized reducer

---
+++ src/store/reducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  comments: [],
+  selectedCommentId: null,
+  selectedComment: null
+};
+
+const reducer = (state = initialState, action) => {
+  return state;
+};
+
+
+export default reducer;
